ccd7ffeffe3f22f2253467c61e29a22015ea4395
KyleMatthews98 <44099824+KyleMatthews98@users.noreply.github.com>
2020-01-08 15:50:00 +0000

    Added Appointment code

--- PatientMGM/src/Appointment/Appointment.java
+++ PatientMGM/src/Appointment/Appointment.java
@@ -1,4 +1,12 @@
 package Appointment;
+import java.time.LocalDateTime;
 
 public class Appointment {
+     int AppointmentID;
+     LocalDateTime startTime;
+     LocalDateTime endTimee;
+     String doctID;
+     String patID;
+     String reason;
+     String notes;
 }
