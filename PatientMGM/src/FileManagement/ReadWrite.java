48e0d8aaf0d5f719e315fbaa673a0791a851ffe0
KyleMatthews98 <44099824+KyleMatthews98@users.noreply.github.com>
2020-01-08 18:56:27 +0000

    Updated Readme

--- PatientMGM/src/FileManagement/ReadWrite.java
+++ PatientMGM/src/FileManagement/ReadWrite.java
@@ -1,15 +1,15 @@
 package FileManagement;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 
 
-public class ReadFile {
-    FileReader data;
+public class ReadWrite {
+
+
 
     protected void open() {
         try {
-            this.data = new FileReader("Login.json");
+            FileReader loginFile = new FileReader("Login.json");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
